5504ee90b96ee8d0b6a0f83d381de12bf49bde2e
KaterinaGerts <75572090+KaterinaGerts@users.noreply.github.com>
2021-10-01 21:49:06 +0300

    добавила трай кэтч

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -1,11 +1,12 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import * as authApi from 'services/auth-api';
+// import axios from 'axios';
 
 export const regicterUser = createAsyncThunk(
   'auth/regicterUser',
-  async (credentials, { rejectWithValue }) => {    
+  async (credentials, { rejectWithValue }) => {
     try {
-      const contacts = await authApi.register(credentials);     
+      const contacts = await authApi.register(credentials);
       return contacts;
     } catch (error) {
       return rejectWithValue(error.message);
@@ -15,9 +16,9 @@ export const regicterUser = createAsyncThunk(
 
 export const logInUser = createAsyncThunk(
   'auth/logInUser',
-  async (credentials, { rejectWithValue }) => {    
+  async (credentials, { rejectWithValue }) => {
     try {
-      const contacts = await authApi.logIn(credentials);     
+      const contacts = await authApi.logIn(credentials);
       return contacts;
     } catch (error) {
       return rejectWithValue(error.message);
@@ -27,9 +28,9 @@ export const logInUser = createAsyncThunk(
 
 export const logOutUser = createAsyncThunk(
   'auth/logOutUser',
-  async (_, { rejectWithValue }) => {    
+  async (_, { rejectWithValue }) => {
     try {
-    await authApi.logOut();       
+      await authApi.logOut();
     } catch (error) {
       return rejectWithValue(error.message);
     }
@@ -39,14 +40,48 @@ export const logOutUser = createAsyncThunk(
 export const saveCurrentUser = createAsyncThunk(
   'auth/saveCurrentUser',
   async (_, thunkAPI) => {
-    const state = thunkAPI.getState();
-    const currentToken = state.auth.token;
+    console.log(thunkAPI);
+    const state = thunkAPI.getState();   
+    const currentToken = state.authorization.token;
     if (currentToken === null) {
       return thunkAPI.rejectWithValue();
     }
-    const data = await authApi.currentUser(currentToken);
-    return data;
+    try {
+      const data = await authApi.currentUser(currentToken);
+      return data;
+    } catch (error) {
+      thunkAPI.rejectWithValue(error.message);
+    }
   },
 );
 
+// axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
+// const token = {
+//   set(token) {
+//     axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+//   },
+//   unset() {
+//     axios.defaults.headers.common.Authorization = '';
+//   },
+// };
+
+// export const saveCurrentUser = createAsyncThunk(
+//   'auth/refresh',
+//   async (_, thunkAPI) => {
+//     const state = thunkAPI.getState();
+//     const persistedToken = state.auth.token;
+
+//     if (persistedToken === null) {//
+//       return thunkAPI.rejectWithValue();
+//     }
+
+//     token.set(persistedToken);
+//     try {
+//       const { data } = await axios.get('/users/current');
+//       return data;
+//     } catch (error) {
+//       // TODO: Добавить обработку ошибки error.message
+//     }
+//   },
+// );
